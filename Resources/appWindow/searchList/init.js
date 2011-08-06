81e9ad29f15c2949098e5abd1027f21126860b25
Peter Eman Abastillas <pabastillas@ltcglobal.com>
2011-08-06 09:05:42 +0800

    updates

--- Resources/appWindow/searchList/init.js
+++ Resources/appWindow/searchList/init.js
@@ -8,19 +8,37 @@ var winSearhList = Titanium.UI.createWindow({
 	navBarHidden: true
 });
 
-var headerView = forms.label("This is the Header",'zipCode',"labelText");
-var footerView = forms.label("This Is The Footer",'zipCode',"labelText");
+var headerView = forms.label("This is the Header",'headerView',"labelText");
+var footerView = forms.label("This Is The Footer",'footerView',"labelText");
 
+// create FormElements;
+var data=[];
+var row = Titanium.UI.createTableViewRow({className :'rowFormSearch'});
+var zipLabel 		= forms.label("Zip Code",'zipLabel',"formLabelText");
+var ageLabel 		= forms.label("Zip Code",'ageLabel',"formLabelText");
+var zipTextBox		= forms.text("00000","zipTextBox","formTextBox");
+var ageTextBox		= forms.text("00", "ageTextBox","formTextBox");
+
+var zipView  		= Titanium.UI.createView({id:"zipView",className:"formSearchView"});
+var ageiew  		= Titanium.UI.createView({id:"zipView",className:"formSearchView"});
+
+genAdd(zipView, [zipLabel,zipTextBox]);
+genAdd(ageiew, [ageLabel,ageTextBox]);
+row[0].add(zipView);
+row[1].add(ageView);
+data.push(row);
 
 var tableView = Titanium.UI.createTableView({
 	data:[],
 	headerView:headerView,
-	footerView:footerView
+	footerView:footerView,
+	id:"searchForm",
+	className:"tableView",
 });
 
 
 
-var optZip 		= forms.label("Zip Code",'zipCode',"labelText");
+// var optZip 		= forms.label("Zip Code",'zipCode',"labelText");
 
 winSearhList.add(optZip);
 winSearhList.open();
--- Resources/libs/tableViewForm.js
+++ Resources/libs/tableViewForm.js
@@ -1,7 +1,16 @@
 var listForm = Class.extend({
 	
 	init: function() {
-		
-	}
+		return this.view();
+	},
 	
+	view : function() {
+		Titanium.UI.createTableView({
+			data:[],
+			id:this.id,
+			className:this.className
+		})
+	},
+	rowData: function(){}
 });
+
